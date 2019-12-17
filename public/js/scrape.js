c1a0ad64cfad51503915bb5e62fe2cd1b1864412
JackRyan1989 <jack.jackryan@gmail.com>
2019-12-17 14:58:51 -0500

    Fixing routes.

--- public/js/scrape.js
+++ public/js/scrape.js
@@ -1,16 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".commBtn").on('click', function(){
+    $(".commBtn").on('click', function () {
         let id = $(this).attr('id');
         
-        $('.comments').append("<textarea class='p-2 w-100' id='bodyInput' name='body'></textarea>");
-        
+        //get the specific article so you can populate the note to the article:
+        $.ajax({
+            method: 'GET',
+            url: '/article/' +id
+        })
+        .then(function(data){
+            console.log(data);
+            $('#' + id + '.comments').append("<input class='p-2 w-50' id='userNameInput' name='user'></input>");
+            $('#' + id + '.comments').append("<textarea class='p-2 w-100' id='bodyInput' name='body'></textarea>");
+            $('#' + id + '.comments').append("<button id=" + id + " class='btn btn-primary saveComm'>Save Comment</button>");
+        })
+    });
 
-
+    $(document).on('click','.saveComm', function () {
+        let commId = $(this).attr('id');
+        console.log(commId);
+        $.ajax({
+            method: 'POST',
+            url: 'article/' + commId,
+            data: {
+                user: $("#userNameInput").val().trim(),
+                body: $("#bodyInput").val()
+            }
+        }).then(function (data) {
+            $("#userNameInput").hide();
+            $("#bodyInput").hide();
+            $(".saveComm").hide();
+        })
     })
-
-
-
-
 
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,54 +13,73 @@ module.exports = function (app) {
     //Just get and display the homepage with articles:
     app.get('/', function (req, res) {
         db.Article.find({})
-        //populate comments:
-        .populate('Comment')
-        .then(function(dbArticles){
-            let articles = {
-                article: dbArticles
-            }
-            res.render('index', articles);
-        })
-        .catch(function(error){
-            res.status(401).json(error);
-    })
+            .then(function (dbArticles) {
+                let articles = {
+                    article: dbArticles
+                }
+                console.log(articles);
+                res.render('index', articles);
+            })
+            .catch(function (error) {
+                res.status(401).json(error);
+            })
     });
-    
+
+    //When we want to comment, you need to grab the specific article you would like to comment on
+    //This path does this:
+    app.get('/article/:id', function(req,res){
+        db.Article.findOne({_id: req.params.id})
+        .populate('note')
+        .then(function(dbArticle){
+            //Just return something:
+            res.json(dbArticle);
+        })
+        .catch(function (error) {
+            res.status(401).json(error);
+        })
+    })
+
     //Do the web scrapin' of Billy Penn, for Articles::
-    app.get('/scrape', function(req, res){
-        axios.get('https://billypenn.com/categories/news/').then(function(response){
+    app.get('/scrape', function (req, res) {
+        axios.get('https://billypenn.com/categories/news/').then(function (response) {
             //Use cheerio to parse the html from Billy Penn:
             let $ = cheerio.load(response.data);
-            $("article h1").each(function(i, element){
+            $("article h1").each(function (i, element) {
                 //Create an object to store our title and link in:
                 const result = {};
 
                 //Populate the text and link from the Billy Penn site to the empty result object:
                 result.title = $(this).children('a').text().trim();
                 result.link = $(this).children('a').attr('href');
-                
+
                 //Create the article in the database:
                 db.Article.create(result)
-                .then(function(dbArticle){
-                    console.log(dbArticle);
-                    //res.render('index', dbArticle);
-                }).catch(function(error){
-                    res.status(401).json(error);
-                })
+                    .then(function (dbArticle) {
+                        console.log(dbArticle);
+                        //res.render('index', dbArticle);
+                    }).catch(function (error) {
+                        res.status(401).json(error);
+                    })
             })
         })
         res.send('Done scrapin\'');
     })
 
     //Post comment to article:
-    app.post('article/:id', function(req, res){
+    app.post('/article/:id', function (req, res) {
         db.Comment.create(req.body)
-        .then(function(dbComment){
-            
-
-        })
-    })
-        
-
+            .then(function (dbComment) {
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
+            })
+            .then(function (dbArticle) {
+                let articles = {
+                    article: dbArticle
+                }
+                res.render('index', articles);
+            })
+            .catch(function (err) {
+                res.json(err);
+            })
+    });
 
 }
